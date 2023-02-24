9d46b7cdd3ef8c765852ff4bcce6afbaf474f7b7
W0ggi <voron529403@mail.ru>
2023-02-25 02:35:42 +0300

    Fixes with pick_character.py

--- menus/pick_character.py
+++ menus/pick_character.py
@@ -1,8 +1,9 @@
 from entities.characters.warrior import Warrior
 from entities.characters.archer import Archer
 from entities.characters.wizard import Wizard
-from misc.consts import CLEAR_SCREEN, HEADER, PROMPT, ATTENTION, NUMERATION, PRESS_ENTER
+from misc.consts import CLEAR_SCREEN, HEADER, PROMPT, ATTENTION, PRESS_ENTER
 from menus.pick_character_name import pick_character_name
+
 
 # A list of available character classes with their respective descriptions
 AVAILABLE_CLASSES = [
@@ -15,8 +16,7 @@ AVAILABLE_CLASSES = [
 def display_classes():
     """Displays the available character classes"""
     # Clear the screen and print the header
-    print(CLEAR_SCREEN)
-    print(f'{HEADER} Выберите класс')
+    print(f'{HEADER} Выберите персонажа: ')
     print()
     # Print the available character classes with their descriptions
     for i, battle_class in enumerate(AVAILABLE_CLASSES):
@@ -33,11 +33,18 @@ def get_class_choice():
         battle_class_index = input(PROMPT)
         print()
         try:
+            # Convert the input to an integer index and check if it's valid
+            index = int(battle_class_index)
+            if index <= 0 or index > len(AVAILABLE_CLASSES):
+                raise ValueError
             # Get the chosen character class based on the index entered
-            battle_class = AVAILABLE_CLASSES[int(battle_class_index) - 1]['class']
+            battle_class = AVAILABLE_CLASSES[index - 1]['class']
         except (ValueError, IndexError):
             # If an error occurs, notify the user and prompt them to try again
             print(f'{ATTENTION} Неверное значение. Повторите попытку...')
+            print()
+            input(f'{PRESS_ENTER} Нажмите Enter для продолжения...')
+            print(CLEAR_SCREEN)
         else:
             # If the input is valid, return the chosen character class
             return battle_class
@@ -47,5 +54,7 @@ def pick_character():
     """Sets player's character"""
     # Get the user's chosen character class
     battle_class = get_class_choice()
+    name = pick_character_name()
+
     # Create a new instance of the chosen character class with the user's name and return it
-    return battle_class(pick_character_name())
+    return battle_class(name)
